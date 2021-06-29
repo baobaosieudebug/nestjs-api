d8d2f48cf9dc77fb5d6218f7e33928049909c3d1
baobaosieudebug <baobaosieudebug@gmail.com>
2021-06-29 15:32:32 +0700

    ref(repository): edit isDeleted to 0

--- src/version/version.repository.ts
+++ src/version/version.repository.ts
@@ -4,27 +4,27 @@ import { VersionEntity } from './version.entity';
 @EntityRepository(VersionEntity)
 export class VersionRepository extends Repository<VersionEntity> {
   getAll(projectId: number) {
-    return this.find({ projectId, isDeleted: null });
+    return this.find({ projectId, isDeleted: 0 });
   }
 
   getById(id: number, projectId: number) {
-    return this.findOne({ id, projectId, isDeleted: null });
+    return this.findOne({ id, projectId, isDeleted: 0 });
   }
 
   getByCode(code: string, projectId: number) {
-    return this.findOne({ code, projectId, isDeleted: null });
+    return this.findOne({ code, projectId, isDeleted: 0 });
   }
 
   async isVersionExistCode(code: string, projectId: number): Promise<boolean> {
     const checkExist = await this.count({
-      where: { code, projectId, isDeleted: null },
+      where: { code, projectId, isDeleted: 0 },
     });
     return checkExist > 0;
   }
 
   async isVersionExistId(id: number, projectId: number): Promise<boolean> {
     const checkExist = await this.count({
-      where: { id, projectId, isDeleted: null },
+      where: { id, projectId, isDeleted: 0 },
     });
     return checkExist > 0;
   }
