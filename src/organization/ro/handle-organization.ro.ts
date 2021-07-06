34427438957ad5598727ca0f158389d69e07ac54
baobaosieudebug <baobaosieudebug@gmail.com>
2021-07-06 10:41:05 +0700

    edit handle RO

--- src/organization/ro/handle-organization.ro.ts
+++ src/organization/ro/handle-organization.ro.ts
@@ -8,4 +8,25 @@ export class HandleOrganizationRO {
 
   @ApiProperty({ type: String })
   code: string;
+
+  @ApiProperty({ type: String })
+  domain: string;
+
+  @ApiProperty({ type: String })
+  logo: string;
+
+  @ApiProperty({ type: String })
+  description: string;
+
+  @ApiProperty({ type: String })
+  address: string;
+
+  @ApiProperty({ type: String })
+  city: string;
+
+  @ApiProperty({ type: String })
+  plan: string;
+
+  @ApiProperty({ type: String })
+  country: string;
 }
--- src/user/users.module.ts
+++ src/user/users.module.ts
@@ -6,19 +6,11 @@ import { UserRepository } from './user.repository';
 import { GroupsModule } from '../group/group.module';
 import { TaskModule } from 'src/task/task.module';
 import { ProjectModule } from '../project/project.module';
-// import { APP_GUARD } from '@nestjs/core';
-// import { RolesGuard } from '../authorization/role.guard';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserRepository]), HttpModule, TaskModule, GroupsModule, ProjectModule],
   controllers: [UsersController],
-  providers: [
-    UsersService,
-    // {
-    //   provide: APP_GUARD,
-    //   useClass: RolesGuard,
-    // },
-  ],
+  providers: [UsersService],
   exports: [UsersService, TypeOrmModule],
 })
 export class UsersModule {}
