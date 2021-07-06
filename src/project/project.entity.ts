b2109dd24175afb42ab9a2c25d876af386549c6a
baobaosieudebug <baobaosieudebug@gmail.com>
2021-07-06 17:34:50 +0700

    ref(project): edit project entity

--- src/project/project.entity.ts
+++ src/project/project.entity.ts
@@ -1,12 +1,15 @@
 import {
   Column,
+  CreateDateColumn,
   Entity,
   JoinColumn,
   JoinTable,
   ManyToMany,
   ManyToOne,
   OneToMany,
+  OneToOne,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 import { OrganizationEntity } from '../organization/organization.entity';
@@ -31,15 +34,41 @@ export class ProjectEntity {
   @Column({ type: 'varchar' })
   code: string;
 
+  @Column({ type: 'varchar', nullable: false })
+  description: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  logo: string;
+
   @Column({ name: 'is_deleted', type: 'varchar', default: 0 })
   isDeleted: number;
 
   @Column({ name: 'organization_id', nullable: true })
   organizationId: number;
 
+  @Column({ name: 'create_by', nullable: true })
+  createById: number;
+
+  @Column({ name: 'admin_id', nullable: true })
+  adminId: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
   @ManyToOne(() => OrganizationEntity, (organization: OrganizationEntity) => organization.projects)
   @JoinColumn({ name: 'organization_id' })
   organization: OrganizationEntity;
+
+  @OneToOne(() => UsersEntity, (user: UsersEntity) => user.project)
+  @JoinColumn({ name: 'create_by' })
+  createBy: UsersEntity;
+
+  @OneToOne(() => UsersEntity, (user: UsersEntity) => user.projectAdmin)
+  @JoinColumn({ name: 'admin_id' })
+  admin: UsersEntity;
 
   @ManyToMany(() => UsersEntity, (user: UsersEntity) => user.projects, {
     cascade: ['insert'],
